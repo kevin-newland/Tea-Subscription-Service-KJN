991b0c4ac46f60896d70c99344f08bc06cc70502
Kevin Newland <kevin.newland@yahoo.com>
2025-03-10 12:44:02 -0500

    created models with their respective associations

---
+++ app/models/subscription.rb
@@ -0,0 +1,10 @@
+class Subscription < ApplicationRecord
+  belongs_to :customer
+  has_many :subscription_teas
+  has_many :teas, through: :subscription_teas
+
+  validates :title, presence: true
+  validates :price, presence: true
+  validates :status, presence: true
+  validates :frequency, presence: true
+end
