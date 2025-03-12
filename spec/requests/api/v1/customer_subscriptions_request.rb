04888d534aa8bf209cc6a01d147a60b74aff7b36
Kevin Newland <kevin.newland@yahoo.com>
2025-03-12 18:20:34 -0500

    added readme, fixed joins controller to take in proper params, fixed routes, added a sad path test. and rescure blocks for error handling

--- spec/requests/api/v1/customer_subscriptions_request.rb
+++ spec/requests/api/v1/customer_subscriptions_request.rb
@@ -6,13 +6,27 @@ RSpec.describe "customer_subscription Endpoints", type: :request do
     subscription = Subscription.create!(title: "Title", price: 10.52, frequency: "Monthly")
     customer_subscription = CustomerSubscription.create!(customer_id: customer.id, subscription_id: subscription.id, is_active: true)
 
-    patch "/api/v1/customer_subscriptions/#{customer_subscription.id}"
+    patch "/api/v1/customer_subscriptions/#{customer.id}/#{subscription.id}"
 
     expect(response.status).to eq(200)
 
     result = JSON.parse(response.body, symbolize_names: true)
 
     expect(result).to have_key(:message)
-    expect(result[:message]).to eq("Cancelation Sucessful")
+    expect(result[:message]).to eq("Cancelation Successful")
+  end
+
+  it "can returns an error when teh record is not found" do
+    customer = Customer.create!(first_name: "Jimmy", last_name: "whocares", email: "seeyainmod3@example.com", address: "123 yay St")
+    subscription = Subscription.create!(title: "Title", price: 10.52, frequency: "Monthly")
+    
+    patch "/api/v1/customer_subscriptions/#{customer.id}/#{subscription.id}"
+    
+    expect(response.status).to eq(404)
+
+    result = JSON.parse(response.body, symbolize_names: true)
+
+    expect(result).to have_key(:error)
+    expect(result[:error]).to eq("Record not found")
   end
 end
