a81ed135a87538284d549e31b774c4139610f87c
Kevin Newland <kevin.newland@yahoo.com>
2025-03-12 14:44:06 -0500

    small changes

--- app/serializers/subscription_serializer.rb
+++ app/serializers/subscription_serializer.rb
@@ -1,11 +1,8 @@
 class SubscriptionSerializer
   include JSONAPI::Serializer
+
   attributes :title, :price, :frequency
 
   has_many :teas
-  has_many :customers
-
-  attribute :customer_subscription_id do |subscription|
-    subscription.customer_subscriptions.first&.id 
-  end
+  has_many :customers, serializer: CustomerSerializer
 end
